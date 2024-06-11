312cf2db15562a0b61f41ee4e4fbb69000a5b34d
Mert Can <mert.kasapoglu@gurok.com>
2024-06-11 12:00:38 +0300

    Dependency Inversion

---
+++ DependencyInversion/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace DependencyInversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dependency Inversion Principle
+            // English explanation
+            // High-level modules should not depend on low-level modules.
+            // Both should depend on abstractions.
+            // Abstractions should not depend on details.
+            // Details should depend on abstractions.
+            // 
+            // Türkçe açıklama
+            // Yüksek seviye modüller düşük seviye modüllere bağlı olmamalıdır.
+            // Her ikisi de soyutlamalara bağlı olmalıdır.
+            // Soyutlamalar ayrıntılara bağlı olmamalıdır.
+            // Ayrıntılar soyutlamalara bağlı olmalıdır.
+            //
+            // Example
+            // We have a class that sends emails.
+            // We have a class that logs messages.
+            // The email sender class should not depend on the logger class.
+            // Both classes should depend on an interface.
+            // The interface should not depend on the details.
+            // The details should depend on the interface.
+
+            ReportNotifiers reportNotifiersM = new ReportNotifiers(new MailSender());
+
+            ReportNotifiers reportNotifiersW = new ReportNotifiers(new WhatsAppSender());
+
+            ReportNotifiers reportNotifiersG = new ReportNotifiers(new GeneralSender());
+            reportNotifiersM.SendReport2();
+            reportNotifiersW.SendReport3();
+            reportNotifiersG.SendReport2();
+            reportNotifiersM.SendReport3();
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DependencyInversion/ReportNotifier.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    public class ReportNotifiers
+    {
+        public INotifier mailSender2;
+        private readonly INotifier mailSender3;
+        public ReportNotifiers(INotifier mailSender)
+        {
+            this.mailSender3 = mailSender;
+        }
+        public void SendReport(INotifier mailSender)
+        {
+            //MailSender mailSender = new MailSender();
+            mailSender.Send();
+            Console.WriteLine("Report sent");
+        }
+        public void SendReport2()
+        {
+            //MailSender mailSender = new MailSender();
+            //mailSender2.Send();
+            Console.WriteLine("Report sent");
+        }
+
+        public void SendReport3()
+        {
+            mailSender3.Send();
+            Console.WriteLine("Report sent");
+        }
+
+
+    }
+
+
+    public class MailSender : INotifier
+    {
+        public void Send()
+        {
+            Console.WriteLine("Mail sent");
+        }
+    }
+
+    public class GeneralSender : INotifier
+    {
+        public void Send()
+        {
+            Console.WriteLine("General sent");
+        }
+    }
+    public class WhatsAppSender : INotifier
+    {
+        public void Send()
+        {
+            Console.WriteLine("WhatsApp sent");
+        }
+    }
+
+    public interface INotifier
+    {
+        void Send();
+    }
+
+}
