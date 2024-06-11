514e97c2c89a0b8f927b404f34e4ca43ef98234f
Mert Can <mert.kasapoglu@gurok.com>
2024-06-11 11:00:57 +0300

    Liskov Finished

---
+++ LiskovSubstitution/Geometry.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution
+{
+    public interface IArea
+    {
+        int GetArea();
+    }
+    public class Geometry
+    {
+        public static IArea GetAreaOfObject(int length1, int? length2=null)
+        {
+            if (length2 == null)
+            {
+                return new Square { Edge = length1 };
+            }
+            else
+            {
+                return new Rectangle { Width = length1, Height = length2.Value };
+            }
+        }
+    }
+
+    public class Rectangle : IArea
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+        public int GetArea() => Width * Height;
+
+    }
+
+    public class Square : IArea //: Rectangle
+    {
+        //private int _width;
+        //private int _height;
+
+        //public override int Width
+        //{
+        //    get => _width;
+        //    set
+        //    {
+        //        _width = value;
+        //        _height = value;
+        //    }
+        //}
+
+        //public override int Height
+        //{
+        //    get => _height;
+        //    set
+        //    {
+        //        _width = value;
+        //        _height = value;
+        //    }
+        //}
+
+        public int Edge { get; set; }
+
+        public int GetArea() => Edge * Edge;
+    }
+}
--- LiskovSubstitution/Program.cs
+++ LiskovSubstitution/Program.cs
@@ -14,8 +14,16 @@
             // Ancak Kare sınıfı, Dikdörtgen sınıfının özelliklerini değiştiremez.
             //  
             // Bu durumda Liskov Substitution Principle'a uymuş oluruz.
-            
-            
+
+            //Square square = new Square();
+            //square.Width = 5;
+            //square.Height = 5;
+            //Console.WriteLine($"Square Area: {square.Area()}");
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = 5;
+            rectangle.Height = 10;
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
 
             Console.WriteLine("Hello, World!");
         }
